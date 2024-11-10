cd7ee0523cf1b3bb6e8c5ceef5ff8fb862e3b045
iislommm <ibragimegamberdiyev81@gmail.com>
2024-11-10 17:38:25 +0500

    6 exercise

---
+++ thirdLessonInFirstModul/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace thirdLessonInFirstModul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter number plz :");
+            //var number = Console.ReadLine();
+
+            //if (number[0] == '-')
+            //{
+            //    Console.WriteLine(number.Length -1);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(number.Length);
+            //}
+            //Console.Write("Enetr number plz :");
+            //var number = int.Parse(Console.ReadLine());
+            //var ones = 0;
+            //var counter = 0;
+
+            //while (true)
+            //{
+            //    ones = number % 10;
+            //    if (ones % 2 == 0) ;
+            //    counter++;
+
+            //    number /= 10;
+            //    if (number == 0)
+            //    {
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(counter);
+
+            //var counter = 0;
+            //while (true)
+            //{
+            //    Console.Write("Enetr number :");
+            //    var number = int.Parse(Console.ReadLine());
+            //    if (number == 0)
+            //    {
+            //        break;
+            //    }
+            //    counter++;
+            //}
+            //Console.WriteLine(counter);
+            //Console.Write("Enter number :");
+            //var number = int.Parse(Console.ReadLine());
+            //var checker = false;
+            //var ones = 0;
+            //while (true)
+            //{
+            //    ones = number % 10;
+            //    if (ones == 5)
+            //    {
+            //        checker = true;
+            //    }
+            //    number /= 10;
+            //    if (number == 0)
+            //    {
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(checker);
+
+            //Console.Write("Enter number :");
+            //var number = int.Parse(Console.ReadLine());
+            //var i = 1;
+            //while (number > i)
+            //{
+            //    i *= 3;
+            //}
+            //if (number == i)
+            //{
+            //    Console.WriteLine("The power of three");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not power of three");
+            //}
+
+
+
+            Main(args);
+        }
+    }
+}
